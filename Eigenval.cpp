cfc6eea6fd546f1938b3abed498da7cb209ad2c1
William Rolen <wrolen@suckcreek.simcenter.utc.edu>
2019-03-18 23:29:26 -0400

    linear alg updates

--- Eigenval.cpp
+++ Eigenval.cpp
@@ -2,40 +2,20 @@
 #include <math.h>
 #include <complex>
 #include <vector>
-#include <bits/stdc++.h>
+#include <valarray>
 const int MAX = 100;
-int mat[MAX][MAX];
+
 using namespace std;
 
-void fillRemaining(int i, int j, int n) 
-{ 
+vector< vector<int> > vec(n, vector<int>(n));
 
-    int x = 2; 
-
-    for (int k = i + 1; k < n; k++) 
-        mat[k][j] = x++; 
-
-    for (int k = 0; k < i; k++) 
-        mat[k][j] = x++; 
-} 
-void fillMatrix(int n) {
-	int right = n-1;
-	int left = 0;
-	for (int i = 0; i < n ; i++) {
-		if (i % 2 ==0) {
-			mat[i][right] = 1;
-			fillRemaining(i, right, n);
-			right--;
-		}
-
-		else {
-			mat[i][left] = 1;
-			fillRemaining(i, left, n);
-			i++;
-		}
-
+void popAndPush(int n) {
+	for (i = 0; i < n; i++) {
+		vec.push_back(rand(), vec);
+		i++
 	}
 }
+
 int main() {
 	int n;
 	cout<<("enter a value for n, please.");
