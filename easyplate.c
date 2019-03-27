63a92981f2666233635976de0a144ebfbb389b4e
smrdafdl <rolenguy@gmail.com>
2019-03-27 16:19:17 -0400

    Update easyplate.c

--- easyplate.c
+++ easyplate.c
@@ -6,8 +6,8 @@
 void plate_simulation(double tol, int NROWS, int NCOLS) {
 	int i, j;
 	double dtmax;
-	double (*plate1)[NCOLS] = malloc(sizeof(double[NROWS][NCOLS]));
-	double (*plate2)[NCOLS] = malloc(sizeof(double[NROWS][NCOLS]));
+	double (*plate_old)[NCOLS] = malloc(sizeof(double[NROWS][NCOLS]));
+	double (*plate_new)[NCOLS] = malloc(sizeof(double[NROWS][NCOLS]));
 	double temp;
 	int source;
 	printf("enter the temp: ");
@@ -17,10 +17,10 @@ void plate_simulation(double tol, int NROWS, int NCOLS) {
 	if (source == 1 || source == 2) {
 		for (j = 0; j<NCOLS;j++) {
 			if (source == 1) {
-				plate1[0][j] = plate2[0][j] = temp;
+				plate_old[0][j] = plate_new[0][j] = temp;
 			}
 			else if (source == 2) {
-				plate1[NROWS-1][j] = plate2[NROWS-1][j] = temp;
+				plate_old[NROWS-1][j] = plate_new[NROWS-1][j] = temp;
 			}
 
 		}
@@ -28,10 +28,10 @@ void plate_simulation(double tol, int NROWS, int NCOLS) {
 	else if (source == 3 || source == 4) {
 		for (i = 0; i<NROWS;i++) {
 			if (source == 4) {
-				plate1[i][0] = plate2[i][0] = temp;
+				plate_old[i][0] = plate_new[i][0] = temp;
 			}
 			else if (source == 3) {
-				plate1[i][NCOLS-1] = plate2[i][NCOLS-1] = temp;
+				plate_old[i][NCOLS-1] = plate_new[i][NCOLS-1] = temp;
 			}
 
 		}
@@ -42,9 +42,9 @@ do {
 	for(i = 1; i < NROWS-1; i++) {
 		for(j = 1; j < NCOLS-1;j++)
 		{
-			plate2[i][j] = (plate1[i-1][j] + plate1[i][j-1] + plate1[i+1][j] + plate1[i][j+1])/4; //physics
-			if (fabs(plate2[i][j] - plate1[i][j]) > dtmax) {
-			dtmax =fabs(plate2[i][j] - plate1[i][j]);
+			plate_new[i][j] = (plate_old[i-1][j] + plate_old[i][j-1] + plate_old[i+1][j] + plate_old[i][j+1])/4; //physics
+			if (fabs(plate_new[i][j] - plate_old[i][j]) > dtmax) {
+			dtmax =fabs(plate_new[i][j] - plate_old[i][j]);
 			}
 		}
 	}
@@ -53,13 +53,13 @@ do {
 	printf("dtmax:%.2f\n",dtmax);
 	for(i = 1;i<NROWS-1;i++) {
 		for(j=1;j<NCOLS-1;j++) {
-			plate1[i][j] = plate2[i][j];
+			plate_old[i][j] = plate_new[i][j];
 		}
 	}
 	} while(dtmax > tol);
 	for(i=0;i<NROWS;i++) {
 		for(j=0;j<NCOLS;j++) 
-		printf("%5.0f", plate1[i][j]);
+		printf("%5.0f", plate_old[i][j]);
 		printf("\n");
 		
 	}
@@ -103,9 +103,24 @@ double random_plate(NROWS, NCOLS) {
 
 	for (int i =0; i<NROWS;i++) {
 		for(int j=0; j<NCOLS; j++) {
-			rand_plate[i][j] = rand();
+			random_plate[i][j] = rand();
 		}
 	}
 	return random_plate;
 }
+
+void plate_print()
+	printf("dtmax:%.2f\n",dtmax);
+	for(i = 1;i<NROWS-1;i++) {
+		for(j=1;j<NCOLS-1;j++) {
+			plate_old[i][j] = plate_new[i][j];
+		}
+	}
+	} while(dtmax > tol);
+	for(i=0;i<NROWS;i++) {
+		for(j=0;j<NCOLS;j++) 
+		printf("%5.0f", plate_old[i][j]);
+		printf("\n");
+		
+	}
 */
