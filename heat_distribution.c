56e3ea55cbb3a494e388eecc4c8e9b3b95475468
William Rolen <wrolen@suckcreek.simcenter.utc.edu>
2019-05-11 20:32:36 -0400

    update

--- heat_distribution.c
+++ heat_distribution.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 //#include <time.h>
 //#include <mpi.h>
+
+#include <time.h>
+#include <mpi.h>
+
 
 
 int idx(int row, int column, int rows, int columns)
@@ -53,7 +58,11 @@ void plate_set_boundary(int NROWS, int NCOLS, double* plate) {
 				plate[idx(i,0, NROWS, NCOLS)] = temp;
 			}
 			else if (source == 3) {
+
 				plate[(i,NCOLS-1, NROWS, NCOLS)] = temp;
+
+				plate[idx(i,NCOLS-1, NROWS, NCOLS)] = temp;
+
 			}
 
 		}
@@ -62,27 +71,45 @@ void plate_set_boundary(int NROWS, int NCOLS, double* plate) {
 
 void plate_simulation(int NROWS, int NCOLS, double *plate, double tol) {
 	// create temporary plate
+
 	double *plate_tmp = plate_creation(NROWS, NCOLS);
 	plate_set_boundary(NROWS, NCOLS, plate_tmp);
 	double *old = plate;
 	double *new = plate_tmp;
 
+
 	// simulate
 	double dtmax = 0.0;
+
+
+
+	// simulate
+	double dtmax = 0.0;
+	double global_dtmax = 0.0;
+
 	do {
 		dtmax = 0.0;
 
 		// simulation step
+
 		for(int i = 1; i < NROWS-1; i++) {
 			for(int j = 1; j < NCOLS-1;j++)
 			{
 				// physics step
 				new[idx(i,j, NROWS, NCOLS)] = (old[idx(i-1,j, NROWS, NCOLS)] + old[idx(i,j-1, NROWS, NCOLS)] + old[idx(i+1,j, NROWS, NCOLS)] + old[idx(i,j+1, NROWS, NCOLS)]) / 4.0;
 
+
+		for(int i = 1; i < NROWS-2; i++) {
+			for(int j = 1; j < NCOLS-1;j++)
+			{
+				// physics step
+				new[idx(i, j, NROWS, NCOLS)] = (old[idx(i-1,j, NROWS, NCOLS)] + old[idx(i,j-1, NROWS, NCOLS)] + old[idx(i+1,j, NROWS, NCOLS)] + old[idx(i,j+1, NROWS, NCOLS)]) / 4.0;
+
 				// change calculation
 				double change = fabs(new[idx(i,j, NROWS, NCOLS)] - old[idx(i,j, NROWS, NCOLS)]);
 				if(change > dtmax) {
 					dtmax = change;
+
 				}
 				/*if ((j % 50) == 0) {
 					MPI_Send(plate_tmp, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
@@ -90,7 +117,20 @@ void plate_simulation(int NROWS, int NCOLS, double *plate, double tol) {
 				else if ((j% 50 != 0)) {
 					MPI_Recv(plate_tmp, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 				}*/
-				
+
+					dtmax = global_dtmax;
+				}
+			//	if (j % 10 == 0) {
+			//	MPI_Send(&new[idx(i,j, NROWS-2, NCOLS)], NCOLS, MPI_DOUBLE, my_top_cart_rank, 0, MPI_COMM_WORLD);
+			//	MPI_Recv(&new[idx(i,j, NROWS-2, NCOLS)], NCOLS, MPI_DOUBLE, my_top_cart_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+			//	MPI_Send(&new[idx(i,j, NROWS, NCOLS -2)], NROWS, MPI_DOUBLE, my_bottom_cart_rank, 0, MPI_COMM_WORLD);
+			//	MPI_Recv(&new[idx(i,j, NROWS, NCOLS -2)], NROWS, MPI_DOUBLE, my_bottom_cart_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+
+			//	}
+
+
 			}
 		}
 
@@ -144,6 +184,78 @@ int main() {
 	//MPI_Comm_size(MPI_COMM_WORLD, &size);
 	//MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+// *NROWS
+int main(int argc, char **argv) {
+	int size;
+	int ndims = 1;
+	int my_rank;
+	MPI_Comm comm = MPI_COMM_WORLD;
+	MPI_Comm Comm1D;
+	int dims[ndims], coord[ndims];
+	int periodic[ndims];
+	int reorder;
+	int ierr;
+	int nrows;
+	int ncols;
+	
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(comm, &size);
+	MPI_Comm_rank(comm, &my_rank);
+
+	printf("rank %i is alive\n", my_rank);
+
+	//if (argc == 3) {
+		//nrows = atoi (argv[1]);
+		//ncols = atoi (argv[2]);
+		/* number of rows */
+		/* number of columns */
+		//if( (nrows*NCOLS) != size) {
+			//if( my_rank ==0) printf("ERROR: nrows*NCOLS)=%d * %d = %d != size\n", NROWS, NCOLS, NROWS*NCOLS,size);
+			//MPI_Finalize(); exit(0);
+		//}
+	//} else {
+	//}
+	// intialize Dimensions
+	//MPI_Dims_create(size, ndims, dims);
+	//if(my_rank==0)
+		//printf("PW[%d], CommSz[%d%]: PEdims = [%d x %d] \n",my_rank,size,dims[0],dims[1]);
+
+	// create cartesian mapping 
+	periodic[0] = 1; // periodic shift is false 
+	reorder = 0;
+
+	dims[0] = size;
+
+	ierr = MPI_Cart_create(MPI_COMM_WORLD, ndims, dims,
+	periodic, reorder, &Comm1D);
+	if(ierr != MPI_SUCCESS) printf("ERROR[%d] creating CART\n",ierr);
+
+	//find my coordinates in the cartesian communicator group 
+	MPI_Cart_coords(Comm1D, my_rank, ndims, coord);
+	//use my cartesian coordinates to find my rank in cartesian group
+
+	printf("rank %i coord %i\n", my_rank, coord[0]);
+
+	int my_top_cart_rank;
+	int my_bottom_cart_rank;
+
+	int coord_top[1];
+	int coord_bottom[1];
+
+	//coord_top[0] = ((coord[0] + 1) + size) ;
+	//coord_bottom[0] = ((coord[0] - 1) + size) % size;
+
+	coord_top[0] = coord[0] + 1;
+	coord_bottom[0] = coord[0] - 1;
+
+	printf("rank %i top %i bottom %i\n", my_rank, coord_top[0], coord_bottom[0]);
+
+	MPI_Cart_rank(Comm1D, coord_bottom, &my_bottom_cart_rank);
+	MPI_Cart_rank(Comm1D, coord_top, &my_top_cart_rank);
+
+	MPI_Comm_free(&Comm1D);
+
 	int NROWS, NCOLS;
 	set_rows_and_cols(&NROWS, &NCOLS);
 
@@ -161,7 +273,14 @@ int main() {
 
 	plate_print(NROWS, NCOLS, plate);
 
+
 	//MPI_Finalize();
+
+	return 0;
+}
+
+
+	MPI_Finalize();
 
 	return 0;
 }
